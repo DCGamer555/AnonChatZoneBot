a83d0172abc1be402e38c07a5594975a5bd5cdfa
Smooothie008 <smooothie008@gmail.com>
2025-07-08 22:59:47 +0530

    update saveNload.py

--- saveNload.py
+++ saveNload.py
@@ -1,19 +1,86 @@
-import os
-import json
+import sqlite3, json, psycopg2, os
 
-FILE_PATH = "user_data.json"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-def save_user_data(user_data: dict):
-    with open(FILE_PATH, "w") as f:
-        json.dump(user_data, f)
-    print("✅ Data saved to disk.")
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
+
+def ensure_db():
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS user_details (
+                    user_id BIGINT PRIMARY KEY,
+                    gender VARCHAR(1),
+                    age INTEGER,
+                    country VARCHAR(25),
+                    reports INTEGER,
+                    reporters TEXT,
+                    vote_up INTEGER,
+                    vote_down INTEGER,
+                    voters TEXT,
+                    feedback_track JSON
+            )
+        """)
+        conn.commit()
+
+def save_user_data(data: dict):
+    with get_connection() as conn:
+        cursor = conn.cursor()
+
+        ensure_db(cursor)
+
+        for user_id, details in data.items():
+            cursor.execute("""
+                    INSERT INTO user_details (
+                        user_id, gender, age, country, reports, reporters, 
+                        vote_up, vote_down, voters, feedback_track
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    ON CONFLICT (user_id) DO UPDATE SET
+                        gender = EXCLUDED.gender,
+                        age = EXCLUDED.age,
+                        country = EXCLUDED.country,
+                        reports = EXCLUDED.reports,
+                        reporters = EXCLUDED.reporters,
+                        vote_up = EXCLUDED.vote_up,
+                        vote_down = EXCLUDED.vote_down,
+                        voters = EXCLUDED.voters,
+                        feedback_track = EXCLUDED.feedback_track,
+            """, (
+                user_id,
+                details.get("gender"),
+                details.get("age"),
+                details.get("country"),
+                details.get("reports", 0),
+                json.dumps(details.get("reporters", [])),
+                details.get("votes", {}).get("up", 0),
+                details.get("votes", {}).get("down", 0),
+                json.dumps(details.get("voters", [])),
+                json.dumps(details.get("feedback_track", {}))
+            ))
+        conn.commit()
 
 def load_user_data() -> dict:
-    if not os.path.exists(FILE_PATH) or os.stat(FILE_PATH).st_size == 0:
-        return {}
-    try:
-        with open(FILE_PATH, "r") as f:
-            return json.load(f)
-    except json.JSONDecodeError:
-        print("⚠️ Warning: userdata.json is corrupted or empty. Starting with a fresh dictionary.")
-        return {}
+    ensure_db()
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM user_details")
+        rows = cursor.fetchall()
+
+        data = {}
+        for row in rows:
+            user_id = row[0]
+            data[user_id] = {
+                "gender": row[1],
+                "age": row[2],
+                "country": row[3],
+                "reports": row[4],
+                "reporters": json.loads(row[5]),
+                "votes": {
+                    "up": row[6],
+                    "down": row[7],
+                },
+                "voters": json.loads(row[8]),
+                "feedback_track": json.loads(row[9]),
+            }
+        return data
