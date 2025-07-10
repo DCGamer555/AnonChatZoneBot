87853ccc80dfad7bf6c12da7350769fd58199cb4
Smooothie008 <smooothie008@gmail.com>
2025-07-10 08:42:20 +0530

    update saveNload.py

--- saveNload.py
+++ saveNload.py
@@ -15,10 +15,10 @@ def ensure_db():
                     age INTEGER,
                     country VARCHAR(25),
                     reports INTEGER,
-                    reporters TEXT,
+                    reporters JSONB,
                     vote_up INTEGER,
                     vote_down INTEGER,
-                    voters TEXT,
+                    voters JSONB,
                     feedback_track JSONB
             )
         """)
